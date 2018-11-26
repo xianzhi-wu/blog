ab5a98f99d29ed98de8c21204804e395cba492ae
xianzhi-wu <405584993@qq.com>
2018-11-26 21:21:22 +0800

    Update index.js

--- js/build/index.js
+++ js/build/index.js
@@ -35,6 +35,11 @@ requirejs(["zepto", "base"], function($, base){
 			},
 			
 	        init : function(){
+			$("#test").on("focus",function(){
+				$(this).prop("type", "number");
+				$(this).prop("type", "text");
+				$(this).val("1234 5678")
+			})		
 	        	var _this = this;
 	        	_this.showArticles();
 	        	
